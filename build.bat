562065bba0fa1a3969720a16c0b93caa1044241f
Remigiusz Schoida <inxaneninja@gmail.com>
2023-03-30 18:04:56 +0200

    Removed build script

--- build.bat
+++
@@ -1,8 +0,0 @@
-cargo build --release
-rmdir /S /Q target\WIN_BUILD
-mkdir target\WIN_BUILD
-copy target\release\tymbark-gui.exe target\WIN_BUILD\tymbark.exe
-copy food.txt target\WIN_BUILD\food.txt
-
-cd target\WIN_BUILD
-tar.exe -a -c -f tymbark-gui.zip tymbark.exe food.txt
